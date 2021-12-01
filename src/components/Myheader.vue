642edb8ce85520db30c3cb815464993f76e598db
Michele Manco <87037954+MicheleManco@users.noreply.github.com>
2021-12-01 14:35:00 +0100

    base iniziale

---
+++ src/components/Myheader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type="text" placeholder="cerca" v-model="query">
+    <button>cerca</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Myheader',
+  data() {
+  return {
+    query: ""
+  }
+},
+created() {
+  console.log(this.query)
+},
+methods:{
+
+
+}
+
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Mymain.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="movie,i in  movies" :key="i">{{movie.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Axios from 'axios'
+
+export default {
+  name: 'Mymain',
+  data() {
+  return {
+    movies: [],
+  }
+},
+created() {
+  this.getmovies()
+},
+methods: {
+  getmovies() {
+    Axios
+    // ${this.query}
+      .get(`https://api.themoviedb.org/3/search/movie?api_key=8792919fa773df65448b9f5dc7714686&language=it-US&query=spiderman`)
+      .then((lista) => {
+        this.movies = lista.data.results
+        })
+  }
+}
+}
+
+
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
